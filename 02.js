ca1d896aa6e9bedc183a05fe87aeb205e75f5c61
adream <tyadream@itcom888.com>
2024-06-02 18:45:30 +0800

    Revert "测试"
    This reverts commit 0280e0b43d1bb2267dc6c57498daab5937cad42a.

--- 02.js
+++ 02.js
@@ -1,12 +1,1 @@
 let b = 2
-
-function filter_percent() {
-  let percentag_danri = this.percent_limit_obj.percentag_dr //单日
-  // 单日过滤值
-  if (percentag_danri && percentag_danri !== 0) {
-    let percent_danri_limit = (percentag_danri * 100).toFixed(2)
-    this.percentageLimit_danri_arr = this.percentageLimit_danri_arr.filter(el => el <=
-      percent_danri_limit
-    )
- }
-}
