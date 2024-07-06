2bab1e27ef1be79c6fd526176da7b75fea341781
adream <tyadream@itcom888.com>
2024-07-06 11:03:54 +0800

    小练习

--- 01.js
+++ 01.js
@@ -1,1 +1,119 @@
-let a = 10
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+localStorage
+sessionStorage
+
+str = "61.222.38.2,61.222.38.4,123.51.197.194,18.166.197.245,10.5.249.2,18.162.167.213,10.5.16.1,103.133.94.18,113.196.86.196,218.32.3.4,10.5.25.104,10.245.204.0,18.162.167.213,172.21.165.97,103.165.81.99,61.222.38.2,61.222.38.4,122.147.54.67,122.147.54.69,103.124.40.2,103.124.40.4,10.5.25.104,10.245.61.192,165.84.166.145,103.165.81.99,10.5.11.46,10.5.35.17,10.5.35.18,10.5.35.16,10.5.35.15,10.5.35.14,10.5.35.13,10.5.35.12,10.5.35.11,10.5.35.26,10.5.35.25,10.5.35.24,10.5.35.23,10.5.34.33,10.5.34.32,10.5.34.31,10.5.34.30,10.5.34.29,10.5.34.28,10.5.34.27,10.5.34.26,10.5.34.25,10.5.34.24,10.5.34.23,10.5.34.22,10.5.34.21,10.5.34.20,10.5.34.19,10.5.34.18,10.5.34.17,10.5.34.16,10.5.34.15,10.5.34.14,10.5.34.13,10.5.34.12,10.5.34.11,10.5.34.10,10.5.33.113,10.5.33.112,10.5.33.111,10.5.33.110,10.5.33.109,10.5.33.108,10.5.33.107,10.5.33.106,10.5.33.105,10.5.33.104,10.5.33.103,10.5.33.102,10.5.33.76,10.5.34.10,10.5.34.11,10.5.34.12,10.5.34.13,10.5.34.14,10.5.34.15,10.5.34.16,10.5.34.17,10.5.34.18,10.5.34.19,10.5.34.20,10.5.34.21,10.5.34.22,10.5.34.23,10.5.34.24,10.5.34.25,10.5.34.26,10.5.34.27,10.5.34.28,10.5.34.29,10.5.34.30,10.5.34.31,10.5.34.32,10.5.34.33,10.5.35.23,10.5.35.24,10.5.35.25,10.5.35.26,10.5.35.11,10.5.35.12,10.5.35.13,10.5.35.14,10.5.35.15,10.5.35.16,10.5.35.17,10.5.35.18,10.5.160.1,10.244.33.108,103.158.160.98,10.105.16.1,103.158.161.78,172.21.165.104,103.84.44.34,10.244.18.201,10.244.18.12"
+
+str_1 = "61.222.38.2,61.222.38.4,123.51.197.194,18.166.197.245,10.5.249.2,18.162.167.213,10.5.16.1,103.133.94.18,113.196.86.196,218.32.3.4,10.5.25.104,10.245.204.0,18.162.167.213,172.21.165.97,103.165.81.99,61.222.38.2,61.222.38.4,122.147.54.67,122.147.54.69,103.124.40.2,103.124.40.4,10.5.25.104,10.245.61.192,165.84.166.145,103.165.81.99,10.5.11.46,10.5.35.17,10.5.35.18,10.5.35.16,10.5.35.15,10.5.35.14,10.5.35.13,10.5.35.12,10.5.35.11,10.5.35.26,10.5.35.25,10.5.35.24,10.5.35.23,10.5.34.33,10.5.34.32,10.5.34.31,10.5.34.30,10.5.34.29,10.5.34.28,10.5.34.27,10.5.34.26,10.5.34.25,10.5.34.24,10.5.34.23,10.5.34.22,10.5.34.21,10.5.34.20,10.5.34.19,10.5.34.18,10.5.34.17,10.5.34.16,10.5.34.15,10.5.34.14,10.5.34.13,10.5.34.12,10.5.34.11,10.5.34.10,10.5.33.113,10.5.33.112,10.5.33.111,10.5.33.110,10.5.33.109,10.5.33.108,10.5.33.107,10.5.33.106,10.5.33.105,10.5.33.104,10.5.33.103,10.5.33.102,10.5.33.76,10.5.34.10,10.5.34.11,10.5.34.12,10.5.34.13,10.5.34.14,10.5.34.15,10.5.34.16,10.5.34.17,10.5.34.18,10.5.34.19,10.5.34.20,10.5.34.21,10.5.34.22,10.5.34.23,10.5.34.24,10.5.34.25,10.5.34.26,10.5.34.27,10.5.34.28,10.5.34.29,10.5.34.30,10.5.34.31,10.5.34.32,10.5.34.33,10.5.35.23,10.5.35.24,10.5.35.25,10.5.35.26,10.5.35.11,10.5.35.12,10.5.35.13,10.5.35.14,10.5.35.15,10.5.35.16,10.5.35.17,10.5.35.18,10.5.160.1,10.244.33.108,103.158.160.98,10.105.16.1,103.158.161.78,172.21.165.104,103.84.44.34,10.244.18.201,10.244.18.12"
+
+
+
+
+
+
+__q_objt|{"appId":10008,"userId":437,"userCode":"shtest","permissions":["","live:scheduling","live:opt:suspendProgram","rcs:scroll:match:op:score:matrix","rcs:quota:user","rcs:report:analyzeoverviewday:view","Ronghe:MatchManager:SportTeam:query","win_loss_game","rcs:early:match:op:manual:concession:agio","rcs:early:sell:early:op","sale:table:xts:click","rcs:report:user:management","merchant:dj:manage:acBonusLog:export","rcs:early:onekey:cancel","rcs:quota:bunch","create:merchant","Ronghe:MatchManager:Team:create:createTeam","manual:deduction","usermanual:deduction","Ronghe:Report:MatchCover:query:page","live:queryList","ep:forbidSpeak:update","Merchant:Manage:key:list","rcs:early:match:op:price:cap","rcs:early:match:play:early:overall","Ronghe:MatchManager:machine:queryMatch","chat:audit:back","order:file:findList","Ronghe:MatchManager:MatchResult:modify:editInfo","Merchant:Manage:notice:cacelNotice","Merchant:Manage:getPassword","live:prepg:enabled","anchor:addAnchor","Ronghe:MatchManager:SportPlayer:query:page","sale:table:switch:view","live:prepg:update","ep:forbidShow:query","ip_white_list_management","Merchant:Order:home:queryBetToday","chat:addChatRoom","rcs:early:match:play:early:auto:margin","merchant:manage:acTask:list","Merchant:Report:match:export","merchant:dj:manage:acTask:add","merchant:dj:manage:entrance:upAcMer","rcs:early:view","rcs:scroll:onekey:cancel","rcs:scroll:match:play:early:overall","Merchant:Manage:notice:backNotice","Merchant:Manage:level:detail","live:opt:playLive","Merchant:Order:user:userDetail","anchor:queryList","Ronghe:MatchManager:MatchResult:modify:confirm","sale:table:xts:view","rcs:settlement:audit:onekey:settlement","Ronghe:MatchManager:MatchScores:modify:ScoresEventOperate","Domain:Manage:save","Ronghe:outrightMatch:updateinfo:toModifyHistory","Ronghe:MatchManager:create:createMatch","Merchant:Finance:closeFinanceDay","merchant:dj:manage:domainLog:view","rcs:scroll:sell:penalty:show","slotMachine:addTicket","anchor:setLanguage","Merchant:Report:user:bet:list","rcs:scroll:sell:early:op","rcs:order:view","Merchant:Finance:exportDay","Merchant:Manage:updateAdmin","rcs:scroll:match:op:manual:win:agio","rcs:scroll:match:op:base:set","sale:table:switch:click","sale:table:sold:view","Merchant:Manage:notice:detail","Ronghe:MatchManager:Tournament:add:addTournament","chat:audit:cleanView","live:table:switch:click","rcs:settlement:audit:confirm","live:opt:set","merchant:dj:manage:acBonusLog:list","chat:audit:view","Report:activity:getActivityBetStatList","Ronghe:MatchManager:MatchResult:query:page","rcs:early:match:op:win:betlimit","live:opt:suspendLive","rcs:early:match:play:early:auto:agio","Merchant:Manage:news:detail","rcs:scroll:match:op:concession:betlimit","Domain:Manage:ossUpload","Merchant:Manage:detail","Merchant:Manage:domain:merchantDomainList","slotMachine:jackpotRecordExport","Merchant:Manage:update","slotMachine:editSlotJackpotCfg","Merchant:Order:home:queryUserToday","rcs:scroll:match:op:overall:mts","rcs:settlement:audit:view","live:prepg:query","rcs:scroll:match:op:auto:win:agio","chat:audit:selectChatRoom","ssv:word:query","rcs:early:match:op:auto:win:margin","Ronghe:MatchManager:match:alarm:view","Ronghe:MatchManager:Tournament:query:export","rcs:early:match:op:win:plunge","ssv:word:edit","slotMachine:addSlotMachineCfg","Ronghe:MatchManager:Tournament:query:page","Ronghe:MatchManager:modify:createManageId","Report:activity:exportExcel","Merchant:Report:merchant:list","rcs:report:user:view","Ronghe:MatchManager:Match:modify","rcs:risk:operate","rcs:quota:play","rcs:early:match:op:manual:win:agio","rcs:early:sell:penalty:show","Merchant:Manage:key:update","rcs:scroll:view","Merchant:Finance:rebootFinanceDay","rcs:early:match:op:auto:win:agio","rcs:early:match:op:concession:plunge","rcs:early:match:play:view","rcs:match:expect:view","rcs:scroll:manual:order","Ronghe:MatchManager:Sale:modify:moveout","ep:forbidShow:remark","merchant:dj:manage:acTask:changeStatus","Ronghe:MatchManager:Sale:modify:auditor\n","OSMC:SETTLE:FREEZE","Merchant:Manage:domain:updateDomainList","chat:audit:showOrder","sets_page:event","rcs:early:match:op:mainmarket:offset","merchant:manage:saveBanner","slotMachine:editTicket","Merchant:Manage:news:add","Ronghe:MatchManager:query:opposite","Merchant:Manage:rate:add","Merchant:Manage:createAdmin","Ronghe:MatchManager:SportTeam:query:export","merchant:manage:acTask:add","Ronghe:MatchManager:SportPlayer:query:export\n","merchant:dj:manage:acTask:changeOrder","Merchant:Manage:domain:updateMerchantGroup","rcs:scroll:match:op:mainmarket:status","rcs:quota:business:once","rcs:scroll:match:op:manual:win:margin","rcs:scroll:match:play:view","rcs:early:match:op:auto:concession:margin","live:table:switch:view","merchant:manage:acTask:changeOrder","Merchant:Report:user:bet:list:export","merchant:dj:manage:entrance:update","Merchant:Finance:operate:list","rcs:early:match:op:base:set","merchant:ty:manage:checktools","rcs:early:sell:corner:show","Ronghe:MatchManager:set:lock","slotMachine:draggableSort","Ronghe:MatchManager:sale:ranking:update","chat:audit:forbidSpeak","Merchant:Manage:news:list","rcs:early:sell:settlement:audit","chat:audit:forbidShow","Ronghe:MatchManager:Tournament:query","activity:delete:all:merchant:activity","rcs:scroll:match:play:early:manual:auto","Merchant:Manage:add","rcs:settlement:audit:onekey:cancel","rcs:early:match:op:manual:concession:margin","Merchant:Manage:merchantAgent:query:list","order:file:execute","rcs:early:sell:view","rcs:scroll:match:op:manual:concession:agio","slotMachine:modifySlotMachineState","Merchant:Manage:level:add","Merchant:Finance:list","Merchant:Manage:notice:edit","slotMachine:resetRecordExport","Merchant:Manage:notice:add","rcs:early:match:op:score:matrix","Merchant:Manage:updateStatus","chat:audit:speak","live:table:xts:click","slotMachine:modifyPropsState","merchant:dj:manage:entrance:upMaCfg","aggregateFestival:update","rcs:scroll:match:op:overall:status","Ronghe:MatchManager:Tournament:modify:setOperatorStatus","rcs:scroll:sell:view","Ronghe:MatchManager:Sale:modify:setPreTrader","Merchant:Report:play:list","aggregateFestival:del","rcs:early:match:play:early:manual:auto","Ronghe:MatchManager:SportPlayer:modify:createManageId","rcs:scroll:match:op:price:cap","Ronghe:MatchManager:SportPlayer:modify:editInfo","merchant:merchant_centre:I&e_merchant:view","rcs:scroll:sell:corner:show","rcs:scroll:match:op:overall:manual:auto","rcs:scroll:immediate:order","sale:table:sold:click","rcs:scroll:sell:settlement:audit","Merchant:Manage:level:list","rcs:settlement:audit:choose:result","Ronghe:MatchManager:Team:modify:setOperatorStatus","merchant:ty:manage:domainSet:view","Ronghe:outrightMatch:create:createOutrightMatch","Merchant:user:updateDisabled","live:table:sold:view","live:table:xts:view","Merchant:Order:user:list","merchant:manage:updateBanner","live:opt:endLive","rcs:scroll:match:op:auto:concession:agio","Ronghe:Report:MatchCover:query:export","aggregateFestival:enableBatch","Ronghe:MatchManager:SportTeam:modify:editInfo","Merchant:Order:user:statistics","rcs:early:match:op:concession:betlimit","Merchant:Manage:news:delete","ep:forbidShow:update","Merchant:Manage:updateKanaCode","chat:queryList","slotMachine:syntheticRecordExport","merchant:manage:acTask:save","rcs:trade:operate","merchant:dj:manage:entrance:upEvDaCfg","Merchant:Order:user:queryHotPlayName","rcs:scroll:match:op:win:plunge","Merchant:Manage:home:list","rcs:award:order:view","Merchant:Order:home:userOrderDay","Merchant:Manage:update:ip","Merchant:Finance:Day:listV2","Merchant:Report:match:list","activity:entrance:config","rcs:scroll:match:op:concession:plunge","Merchant:Manage:home:queryAgentCount","Merchant:Manage:domain:createMerchantGroup","Ronghe:MatchManager:SportTeam:query:page","Merchant:Manage:level:update","merchant:dj:manage:entrance:del","rcs:early:match:op:auto:concession:agio","live:prepg:del","merchant:manage:getBanners","rcs:early:match:op:overall:mts","activity:update:all:merchant:activity","rcs:report:user:label","slotMachine:modifyTicketState","rcs:report:matchorderday:view","Ronghe:MatchManager:SportTeam:modify","merchant:dj:manage:acTask:save","rcs:settlement:audit:modify","rcs:report:matchplayday:view","Merchant:Manage:notice:list","Ronghe:outrightMatch:create:createOutrightMarket","Merchant:Manage:key:get","Merchant:Manage:list","Merchant:Manage:notice:update","merchant:dj:manage:domainSet:view","Merchant:Report:merchant:list:export","Merchant:Manage:home:merchantOrderTop","rcs:scroll:match:op:auto:win:margin","Ronghe:MatchManager:query:detail","Merchant:Manage:rate:detail","rcs:early:sell:market:num","rcs:scroll:match:op:win:betlimit ","aggregateFestival:add","pd_advanced_privileges","merchant:manage:deleteBanner","Merchant:Order:home:userDaySpread","Ronghe:MatchManager:Sale:modify:setLiveTrader\n\n\n\n","live:table:preTrader:click","merchant:manage:acTask:changeStatus","activity:config","merchant:dj:manage:acTask:list","Ronghe:outrightMatch:create:modifyBuildOutrightMatch","Merchant:Manage:notice:delete","slotMachine:saveSlotMachineProps","Merchant:Order:user:bet:export","Merchant:user:importToDisabled","Ronghe:MatchManager:update:updateTournamentMatchBeginTime","activity:update:all:merchant:entrance","Merchant:Manage:merchantAgent:batchAdd","Merchant:Finance:detail","chat:enabled","ssv:word:add","slotMachine:editProps","Ronghe:MatchManager:match:alarm:operate","rcs:scroll:sell:market:num","merchant:ty:manage:domainLog:view","merchant:cp:manage:domainLog:view","rcs:scroll:match:play:early:auto:margin","rcs:scroll:match:op:manual:concession:margin","Merchant:Order:user:list:export","merchant:cp:manage:domainSet:view","merchant:manage:acBonusLog:list","rcs:early:match:op:overall:manual:auto","slotMachine:addProps","rcs:early:match:op:overall:status","order:file:deleteFile","Merchant:Finance:Day:Totallist","Merchant:Manage:updateDisabled","merchant:dj:manage:entrance:addAcMer","live:opt:playProgram","Merchant:Manage:domain:deleteMerchantGroup","slotMachine:editSlotMachineCfg","Merchant:Order:user:transferList","merchant:manage:bonusRecord:reissueBonus","anchor:updateState","Ronghe:MatchManager:Sale:query:page","Ronghe:MatchManager:OptLog:query:page","live:table:preTrader:view","rcs:early:manual:order","rcs:early:match:op:mainmarket:status","Ronghe:MatchManager:Sale:online:insert","Ronghe:MatchManager:MatchResult:modify:amend","Merchant:Finance:operate:update","rcs:scroll:match:op:settlement:audit:view","Ronghe:MatchManager:modify:createManageId\n","rcs:report:matchuserday:view","Merchant:Manage:rate:list","Merchant:Order:user:bet:list","ep:forbidSpeak:remark","merchant:merchant_centre:test_merchant:view","rcs:scroll:match:play:early:auto:agio","rcs:order:handle","Merchant:Order:home:matchTop","rcs:scroll:match:op:auto:concession:margin","Ronghe:MatchManager:query:page","merchant:dj:manage:entrance:upBanner","Merchant:Finance:Day:list","rcs:quota:view","Merchant:Manage:level:query:list","rcs:early:match:op:mainmarket:manual:auto","Merchant:Order:home:userDay","live:shd:queryList","rcs:early:immediate:order","rcs:early:match:op:manual:win:margin","merchant:manage:acBonusLog:export","Merchant:Manage:domain:cleanMerchant","Ronghe:outrightMatch:modify:setOutrightSell","rcs:scroll:match:op:mainmarket:offset","rcs:quota:play:once","Ronghe:MatchManager:machine:auditMatch","live:prepg:add","order:file:export","rcs:early:match:op:settlement:audit:view","rcs:quota:business:day","Merchant:Manage:domain:selectMerchantGroup","Ronghe:MatchManager:Tournament:modify:editInfo","Ronghe:MatchManager:Tournament:modify"],"loginCode":"20240616081632","module":["商户注单统计","期数管理","主播管理","直播班表","公司代码设置","全局接口配置","数据同步工具","活动投注统计","商户等级设置","商户域名管理","用户投注额度查询","首页","赛事直播","注单查询es实时流","商户等级设置2","内容审核","域名设置","用户投注统计","直播管理","盲盒奖品设置","聊天室列表","新操盘中心","数据中心","胜负游戏管理","商户","文章数据","标签管理","奖池详情","作者管理","敏感词管理","直播列表","文章管理","我的导出任务","对账商户投注统计","运营管理","奖券派发记录","任务中心","消息中心","赛事投注统计","道具管理","内容管理","游戏配置","平台费率设置","对账单","投注用户管理","商户APP域名管理","游戏记录","域名切换日志","赛前节目","注单查询ES","设置中心","采集管理","注单查询","投注用户白名单","商户组","二次结算查询","广告位管理","代理商管理","商户中心","奖金池管理","任务设置","奖券管理","赛程","系统级别开关","脱敏设置","盲盒领取记录","视频控制管理","运营位","清算管理","IP白名单管理","财务中心","异常IP池管理","商户管理","异常用户名单","对账工具","公告栏","证书管理","运营活动设置","商户组域名查询","交易记录查询","异常用户查询","交易记录查询","VIP注单下载","即时赛事统计报表","操作日志查询","我的消息","赛事文章","前端域名查询","活动账变记录","注单明细","资源配置","域名池管理"],"useFtpUrl":"123456"}
+
+
+
+
+
+
+
+
+str2 = [
+  "10.5.35.17",
+  "10.5.35.18",
+  "10.5.35.15",
+  "10.5.35.16",
+  "10.5.11.46",
+  "10.245.61.192",
+  "122.147.54.69",
+  "122.147.54.67",
+  "10.5.33.109",
+  "10.5.35.13",
+  "10.5.33.108",
+  "10.5.33.76",
+  "10.5.35.14",
+  "10.5.33.107",
+  "10.5.35.11",
+  "10.5.33.106",
+  "10.5.35.12",
+  "10.5.33.105",
+  "10.5.33.104",
+  "10.5.33.103",
+  "103.158.160.98",
+  "10.5.33.102",
+  "10.244.33.108",
+  "103.84.44.34",
+  "10.105.16.1",
+  "10.5.249.2",
+  "172.21.165.97",
+  "10.5.160.1",
+  "10.5.34.31",
+  "123.51.197.194",
+  "10.5.34.30",
+  "10.5.34.33",
+  "10.5.34.32",
+  "10.5.16.1",
+  "218.32.3.4",
+  "103.133.94.18",
+  "10.5.25.104",
+  "103.158.161.78",
+  "10.5.34.26",
+  "10.245.204.0",
+  "10.5.34.25",
+  "10.5.34.28",
+  "10.5.34.27",
+  "10.244.18.12",
+  "61.222.38.2",
+  "10.5.34.29",
+  "61.222.38.4",
+  "10.244.18.201",
+  "10.5.34.20",
+  "10.5.34.22",
+  "10.5.34.21",
+  "10.5.34.24",
+  "10.5.34.23",
+  "10.5.34.15",
+  "10.5.34.14",
+  "10.5.35.26",
+  "10.5.34.17",
+  "10.5.34.16",
+  "18.162.167.213",
+  "10.5.34.19",
+  "10.5.34.18",
+  "103.124.40.2",
+  "165.84.166.145",
+  "172.21.165.104",
+  "103.124.40.4",
+  "10.5.35.24",
+  "10.5.34.11",
+  "10.5.35.25",
+  "10.5.34.10",
+  "103.165.81.99",
+  "10.5.34.13",
+  "10.5.35.23",
+  "10.5.34.12",
+  "113.196.86.196",
+  "10.5.33.113",
+  "10.5.33.112",
+  "18.166.197.245",
+  "10.5.33.111",
+  "10.5.33.110"
+]
--- 02.js
+++ 02.js
@@ -1,1 +1,21 @@
 let b = 2
+
+new Promise((resolve, reject) => {
+  setTimeout(() => {
+    try {
+      const a = 1
+      a = 2
+      throw Error("错误结果");
+    } catch (error) {
+      console.log(error);
+    }
+  }, 0);
+}).then(result => {
+  console.log(result);
+}).catch(error => {
+  console.log(error);
+})
+
+
+
+
